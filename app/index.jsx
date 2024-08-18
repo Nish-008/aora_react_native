a4c280455a9d185d167cb8a45f36bcc817a8b327
Nish-008 <farihazaman.nishat@gmail.com>
2024-08-18 19:45:42 +0600

    tailwind working

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 const RootLayout = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Aora!</Text>
+      <Text className="text-3xl">Aora!</Text>
       <StatusBar style='auto'/>
       <Link href='/profile' style={{color: 'blue'}}>Go to Profile</Link>
     </View>
