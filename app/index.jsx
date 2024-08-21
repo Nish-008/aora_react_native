21df7f02d1598dd66f46bf7e9e1ec300086e17d1
Nish-008 <farihazaman.nishat@gmail.com>
2024-08-21 20:57:59 +0600

    obboarding screen 3

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Text, View, Image } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router';
+import { Redirect, router} from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -14,7 +14,7 @@ const RootLayout = () => {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaView className="bg-primary h-full"> 
         <ScrollView contentContainerStyle={{height:'100%'}}>
-          <View className="w-full justify-center items-center h-full px-4">
+          <View className="w-full justify-center items-center min-h-[85vh] px-4">
             <Image
               source={images.logo}
               className="w-[130px] h-[84px]"
@@ -37,9 +37,14 @@ resizeMode='contain'
 />
             </View>
             <Text className="text-sm font-pregular text-gray-100 mt-7 text-center"> Where creativity meets innovation: embark on a journey on limitless exploration with Aora</Text>
-          <CustomButton/>
+          <CustomButton
+          title="Continue with Email"
+          handlePress={() => router.push('/sign-in')}
+          containerStyles="w-full mt-7"
+          />
           </View>
         </ScrollView>
+        <StatusBar backgroundColor='#161622' style='light'/>
       </SafeAreaView>
     </GestureHandlerRootView>
   )
