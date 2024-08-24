6b554f4a4fe3bfa3e7dc5866e46f1c08c11fd98e
Nish-008 <farihazaman.nishat@gmail.com>
2024-08-24 23:29:01 +0600

    appwrite logic 1

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import {images} from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { createUser } from '../../lib/appwrite';
 
 const SignUp = () => {
@@ -17,7 +17,21 @@ const SignUp = () => {
 
   const [isSubmitting, setisSubmitting] = useState(false)
 
-  const submit = () => {
+  const submit = async () => {
+    if(!form.username || !form.email || !form.password ){
+      Alert.alert('Error','Please fill in all the fields')
+    }
+
+    setisSubmitting(true);
+    try {
+      const result= await createUser(form.email,form.password, form.username)
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setisSubmitting(false)
+    }
     createUser();
   }
 
@@ -52,7 +66,7 @@ handleChangeText={(e)=> setForm({ ...form,password:e})}
 otherStyles="mt-7"
 />
 <CustomButton
-title="Sign-In"
+title="Sign-Up"
 handlePress={submit}
 containerStyles="mt-7"
 isLoading={isSubmitting}
