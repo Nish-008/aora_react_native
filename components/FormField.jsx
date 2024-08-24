ab8b159ba612d58d51122808883e4ba704a03300
Nish-008 <farihazaman.nishat@gmail.com>
2024-08-24 20:37:55 +0600

    eye icon fixed

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -12,7 +12,7 @@ const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ..
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
       <View
-      className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-secondary"
+      className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row"
       >
         <TextInput
         className="flex-1 text-white font-psemibold text-base"
@@ -23,12 +23,13 @@ const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ..
         secureTextEntry={title === 'Password' && !showPassword}
         />
 {title === 'Password' && (
-  <GestureHandlerRootView style={{ flex: 1 }}>
+  <GestureHandlerRootView>
    <TouchableOpacity onPress={()=> setshowPassword(!showPassword)}>
 <Image
 source={!showPassword ? icons.eye : icons.eyeHide}
 className="w-6 h-6"
 resizeMode='contain'
+style={{ marginLeft: 'auto' }}
 />
     </TouchableOpacity>
     </GestureHandlerRootView>
